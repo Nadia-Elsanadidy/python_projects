98a3395c34571c1edefd82df26c38565af7391d6
Nadia-Elsanadidy <nadiaa.amrr@gmail.com>
2025-05-06 22:25:35 +0300

    Add files via upload

---
+++ All.py
@@ -0,0 +1,25 @@
+
+import multiplication_table1
+import count_vowels
+import mario_pyramid1
+import sort_numbers
+import mario_pyramid2
+import multiplication_table2
+import check_name_email
+import check_users
+
+#Day 1
+multiplication_table1.multiplication_table1()
+count_vowels.count_Vowels()
+mario_pyramid1.Mario_Pyramid1()
+
+#Day 2
+sort_numbers.sort_numbers()
+mario_pyramid2.mario_pyramid2()
+multiplication_table2.multiplication_table2()
+
+#Day 3
+check_name_email.check_name(check_name_email.name)
+check_name_email.check_email(check_name_email.email)
+check_users.check_users(check_users.user_name)
+check_users.check_users(check_users.password)
